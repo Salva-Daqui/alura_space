cd40f771dd4c99b8ec9e69fa8c6a7303292a3204
Salvador Soares <m273330@dac.unicamp.br>
2025-09-17 02:53:46 -0300

    importing views

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from galeria.views import index
+from galeria import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
