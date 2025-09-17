1bbc6c2588f293903cb081a75b68aea0d026e3da
Salvador Soares <m273330@dac.unicamp.br>
2025-09-17 07:02:46 -0300

    back home works

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from galeria import views
 
-urlpatterns = [
-    path('', views.index),
-    path('imagem/', views.img),
+urlpatterns = [ 
+    path('', views.index, name = 'home'),
+    path('imagem/', views.img, name = 'imagem'),
 ]
