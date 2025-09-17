447627bd9c3dd6cd8c3a1b275943d03494af7e5e
Salvador Soares <m273330@dac.unicamp.br>
2025-09-17 03:35:29 -0300

    templates.exibition

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from galeria import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', include('galeria.urls')),
 ]
---
+++ galeria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from galeria import views
+
+urlpatterns = [
+    path('', views.index)
+
+    
+]
