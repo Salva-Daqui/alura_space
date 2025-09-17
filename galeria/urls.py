5f1f2fae27570f3aacecc8ebbe79301ab365f13b
Salvador Soares <m273330@dac.unicamp.br>
2025-09-17 06:12:58 -0300

    end of 4

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from galeria import views
 
 urlpatterns = [
-    path('', views.index)
-
-    
+    path('', views.index),
+    path('imagem/', views.img),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galeria/exibition.html')
+
+def img(request):
+    return render(request, 'galeria/imagem.html')
