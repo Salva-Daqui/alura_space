5b260321b86e5fb4bfab7c43b7c55c663d7e6257
Salvador Soares <m273330@dac.unicamp.br>
2025-10-28 21:41:39 -0300

    preparando monitoria

---
+++ itt-unicamp/aula1.py
@@ -0,0 +1,11 @@
+from bib_do_salva import function1, function2, function3
+
+x = int(input(""))
+y = function1(x)
+print(f"Responde rápido: Quanto é {x} vezes 4??\n")
+resposta = int(input(""))
+function3(resposta, function2(y))
+
+
+
+
---
+++ itt-unicamp/bib_do_salva.py
@@ -0,0 +1,21 @@
+def function1(x):
+    print("Salve galera do ITT!")
+    print("Tudo bem com vcs? :)")
+    return x * 2
+
+
+
+
+def function2(x):
+    return x * 2
+
+
+
+
+
+def function3(resposta, valor):
+    if resposta == valor:
+        print(f"Boa! A resposta é {valor} mesmo!")
+
+    else:
+        print(f"Pensou rápido demaiskkkkkk, a resposta correta é {valor}")
